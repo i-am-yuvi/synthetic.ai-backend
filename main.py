30cac8d8b35757060c52db268cda078e8db72c94
Yuvraj Chhetri <yuvichh01@gmail.com>
2023-05-22 15:19:19 +0530

    updated the port

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from nltk.tokenize import word_tokenize
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+app.run(
+    host="0.0.0.0",
+    port=5000
+)
 CORS(app)
 
 @app.route('/generate', methods=['POST'])
